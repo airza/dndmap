76fbc3e7b722cab037f9d73a085b3da683cbb9bf
Glitch (hello-sqlite) <none>
2019-02-01 22:05:47 +0000

    🏹🤗 Checkpoint ./babel.config.js:299832/74 ./webpack.config.js:299832/161 ./package.json:299832/52 ./public/style.css:299832/3595

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = ["@babel/plugin-transform-react-jsx",["react-css-modules",{
-      "option": "value"
-    }]];
-  const presets = [];
+  const plugins = ["@babel/plugin-transform-react-jsx", "react-css-modules"];
   return {
-    presets,
     plugins
   };
 }
