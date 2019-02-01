83b13e2184f67269b9fc90fb7e85dd2a5962f2d7
Glitch (hello-sqlite) <none>
2019-02-01 20:40:06 +0000

    ⛈🍐 Checkpoint ./webpack.config.js:299832/331 ./package.json:299832/226 ./src/App.css:299832/627 ./views/index.html:299832/269

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/client.jsx',
+  output: {
+    filename: 'client.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+  rules: [
+    { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }
+  ]
+}
+};
